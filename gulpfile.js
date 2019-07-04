14ff183a756b5b1de7cd4b2dca31571c32a78c6b
강대한 <kdh7565@sk.com>
2019-07-04 15:43:16 +0900

    * 이미지 압축 task 추가

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,13 @@ var del = require('del');
 // var autopolyfiller = require('gulp-autopolyfiller');
 var spritesmith = require('gulp.spritesmith');
 var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var changed = require( 'gulp-changed');
 var cache = require('gulp-cache');
+var cached = require('gulp-cached');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminGiflossy = require('imagemin-giflossy');
+var imageminMozjpeg = require('imagemin-mozjpeg');
 
 // 폴더 정의
 var src = 'ui'; //작업 폴더
@@ -39,6 +45,32 @@ var tmp = {
   html: temp + '/**/*.html'
 };
 
+// Clean Sprite
+gulp.task('clean-sprite', async function() {
+	return del('./dist/**/*.+(png|jpg|jpeg|gif|svg)');
+});
+
+// image sprite
+gulp.task('sprite', async function(){
+	var spriteData = gulp.src('../gulp-test/ui/img/*.png').pipe(spritesmith({
+		imgName: 'sprite.png',
+		cssName: 'sprite.css',
+		cssTemplate: null,
+		// cssSpritesheetName: sprite,
+		padding: 10,
+		imgPath: './ui/common/img/sprite.png',
+		cssFormat: 'css',
+		cssVarMap: function (sprite) {
+			sprite.name = `sprite-${sprite.name}`;
+		}
+		// retinaSrcFilter: './dev/data/sprite/*@2x.png',
+		// retinaImgName: 'sprite@2x.png',
+		// retinaImgPath: '../images/common/sprite@2x.png'
+	}));
+	spriteData.img.pipe(gulp.dest('../gulp-test/ui/common/img/'));
+	spriteData.css.pipe(gulp.dest('../gulp-test/ui/common/scss/'));
+});
+
 // scss 파일을 css 로 변환
 // task 순서 : sourcemap 생성 > sass 실행 > sourcemap 표시 > dist로 복사 > browser reload
 // async completion 오류 해결 방법 task function에 async 추가
@@ -58,7 +90,7 @@ gulp.task('sass', async function() {
 gulp.task('watch', async function(){
   gulp.watch(path.scss, gulp.series(['sass']));
   gulp.watch(path.js, gulp.series(['copyJs']));
-  gulp.watch(path.img, gulp.series(['copyImg','sprite']));
+  gulp.watch(path.img, gulp.series(['sprite','copyImg']));
   gulp.watch(path.html, gulp.series(['html']));
 });
 
@@ -97,26 +129,6 @@ gulp.task('copyJs', async function() {
   }))
 });
 
-// image sprite
-// 이미지 
-gulp.task('sprite', async function(){
-	var spriteData = gulp.src('../gulp-test/ui/img/*.png').pipe(spritesmith({
-		imgName: 'sprite.png',
-		cssName: '_sprite.scss',
-		padding: 10,
-		imgPath: './ui/common/img/sprite.png',
-		// cssFormat: 'scss',
-		cssVarMap: function (sprite) {
-			sprite.name = 'sprite-' + sprite.name;
-		}
-		// retinaSrcFilter: './dev/data/sprite/*@2x.png',
-		// retinaImgName: 'sprite@2x.png',
-		// retinaImgPath: '../images/common/sprite@2x.png'
-	}));
-	spriteData.img.pipe(gulp.dest('../gulp-test/ui/common/img/'));
-	spriteData.css.pipe(gulp.dest('../gulp-test/ui/common/scss/'));
-});
-
 // img copy
 gulp.task('copyImg', async function() {
   return gulp.src(path.img)
@@ -127,36 +139,31 @@ gulp.task('copyImg', async function() {
 });
 
 // img build
-// 이미지 빌드시 파일 사이즈 압축
+// 이미지 무손실 용량 압축 실행 후 배포 폴더로 이동
 gulp.task('buildImg', async function() {
-  return gulp.src(path.img)
-  .pipe(cache(
+	return gulp.src(path.img)
+	.pipe(
 		imagemin([
 			imagemin.gifsicle({interlaced: true}),
 			imagemin.jpegtran({progressive: true}),
-			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.optipng({optimizationLevel: 7}),
 			imagemin.svgo({
 				plugins: [
-					{removeViewBox: true},
-					{cleanupIDs: false}
+					{removeViewBox: false}
 				]
 			})
 		])
-	))
-  .pipe(gulp.dest(dist))
+	)
+	.pipe(gulp.dest(dist))
 });
 
 // useref task 추가
 gulp.task('useref', async function(){
   return gulp.src(tmp.html)
-    .pipe(useref({searchPath:temp}))  // 파일 통합의 기준은 temp 폴더를 기준으로
-    .pipe(gulpIf('*.js', uglify()))   // js 파일의 경우 uglify
-	.pipe(gulpIf('*.css', cssnano())) // css 파일은 cssnano 로 압축하여
-    .pipe(gulp.dest(dist))  // dist 폴더로 이동
-});
-
-gulp.task('clear', async function () {
-	return cache.clearAll();
+    .pipe(useref({searchPath:temp}))  // 파일 통합의 기준은 temp 폴더
+    .pipe(gulpIf('*.js', uglify()))   // js 파일 uglify로 압축
+	.pipe(gulpIf('*.css', cssnano())) // css 파일 cssnano로 압축
+    .pipe(gulp.dest(dist))	// dist 폴더로 이동
 });
 
 // clean
@@ -166,7 +173,7 @@ gulp.task('clean:dist', async function() {
 
 // default 
 gulp.task('default', gulp.series(
-	gulp.parallel('sass', 'copyJs', 'html', 'watch', 'copyImg', 'sprite'),
+	gulp.parallel('sass','copyJs', 'html', 'watch', 'copyImg', 'sprite'),
 	'browserSync'
 ));
 
